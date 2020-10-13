ed60ed0bc7334e5210c842eaa81cb8ff69de69ee
CarolY <mon_alisa75@yahoo.com>
2020-10-13 11:37:50 -0700

    Re-write getweather function

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
-var cityName = [];  
+var citySearches = [];  
+var city;
+var lat;
+var lon;
+var searchBtn = $("#addCityBtn");
+var searchFrield = $("searchCity");
 
 
+function setup(){
 
-//how to get the value for city??
+    searchBtn.on("click", function(event) {
+
+        event.preventDefault();
+    
+        city = $("#searchCity").val().trim();
+        citySearches.push(city);
+    
+                 
+        setCitiesToLocalStorage();
+        getCurrentWeather(city);
+        getFiveDayForecast(city);
+        displayPastCitySearchList();
+
+    
+    });
+    getCitiesFromLocalStorage();
+}
+
+function displayPastCitySearchList(){
+
+    $("#buttonView").empty();
+
+    for (var i=0; i < citySearches.length; i++) {
+        var li = $("<li>");
+        var btn = $("<button>");
+        btn.addClass("city-btn");
+        btn.attr("data-city", citySearches[i]);
+        btn.text(citySearches[i]);
+        $("#buttonView").append(li); 
+        li.append(btn);
+    }    
+}
+
+function getCitiesFromLocalStorage() {
+    if (localStorage.getItem('cities')) {
+      citySearches = JSON.parse(localStorage.getItem('cities'));
+    }
+  }
+
+  // Saves citySearches array to local storage
+function setCitiesToLocalStorage() {
+    // TODO: Call setItem() of localStorage
+    localStorage.setItem("cities", JSON stringify(citySearches));
+  }
 
 
+function getCurrentWeather(){
 
-function displayWeather(){
-    //BROKEN----------------
     var city = $(this).attr("data-city");
-    // console.log(city);
     const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=3b39c2827e08627d2c1ebcae6181db52";
-//----------------------------------------------
+    //----------------------------------------------
     $.ajax({
         url: queryUrl,
         method: "GET"
@@ -28,6 +75,11 @@ function displayWeather(){
         var temperature = convertKtoF(parseFloat(response.main.temp)) + "&deg;F";
         var pTwo = $("<p>").text("Temperature" + temperature);
         weatherDiv.append(pTwo);
+
+        function convertKtoF(tempInKelvin) {
+            return(((tempInKelvin-273.15)*9)/5 + 32).toFixed(2);
+        }
+       
         
         //humidity
         var humidity = response.mail.humidity;
@@ -42,8 +94,8 @@ function displayWeather(){
 
         //UV index, another ajax call
 
-        var lat= response.coord.lat;
-        var lon= esponse.coord.lon;
+        lat = response.coord.lat;
+        lon = esponse.coord.lon;
 
         const queryUvi = "http://api.openweathermap.org/data/2.5/uvi?lat="+ lat + "&lon="+ lon + "&appid=3b39c2827e08627d2c1ebcae6181db52";
 
@@ -58,68 +110,35 @@ function displayWeather(){
         var pFour = $("<p>").text("UV Index: " + uvIndex);
         weather.append(pFour);
 // need to do if statement for color code: green(<2), yellow(3-5), orange(6-7), red(8-10), purpple(>11)
-
-
         })
 
         $("#cityWeather").append(weatherDiv);
+    });
+}
 
-    function convertKtoF(tempInKelvin) {
-        return(((tempInKelvin-273.15)*9)/5 + 32).toFixed(2);
-    }
+
+function getFiveDayForecast(){
     
-
-   //------------5-day forcast function----------
-var cityId = response.coord.id;
-const apiForecast = "http://api.openweathermap.org/data/2.5/forecast/daily?id="+cityId+"&cnt=5&appid=3b39c2827e08627d2c1ebcae6181db5";
-
-$.ajax({
-    url: apiForecast,
-    method: "GET"
-
-}).then(function(forecast) {
-    console.log(forecast);
-
-//the card need to have current date --- can we use moment? any better way?
-//weather icon 
-//temp. ----
-//Humidity---
-//--------------------------------------
-
+    const apiForecast = "http://api.openweathermap.org/data/2.5/forecast/daily?id="+cityId+"&cnt=5&appid=3b39c2827e08627d2c1ebcae6181db5";
+    
+    $.ajax({
+        url: apiForecast,
+        method: "GET"
+    
+    }).then(function(forecast) {
+        console.log(forecast);
+    
+    //the card need to have current date --- use moment? 
+    //weather icon 
+    //temp. ----
+    //Humidity---
+    //--------------------------------------
+    
     });
 
-    
-}
 
-function renderButtons(){
-
-    $("#buttonView").empty();
-
-    for (var i=0; i < cityName.length; i++) {
-        var li = $("<li>");
-        var btn = $("<button>");
-        btn.addClass("city-btn");
-        btn.attr("data-city", cityName[i]);
-        btn.text(cityName[i]);
-        $("#buttonView").append(li); 
-        li.append(btn);
-    }    
 }
 
 
-//click function to call 
-$("#addCitybtn").on("click", function(event) {
+  
 
-    event.preventDefault();
-
-    var city = $("#searchCity").val().trim();
-    cityName.push(city);
-
-    displayWeather();
-    renderButtons();
-   
-
-});
-
-$(document).on("click", "city-btn", displayWeather);
-renderButtons();
